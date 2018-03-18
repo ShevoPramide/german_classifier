754d460b62fbb9f3806413d23202dacc2a86f58a
omar <omelgazzar@gmail.com>
2018-03-18 20:43:35 +0200

    Added database support and remove functionality

--- db_helpers.py
+++ db_helpers.py
@@ -1,8 +1,33 @@
-data = {}
+import sqlite3
+from collections import namedtuple
+
+_db = "german.sqlite"
+
+Word = namedtuple('Word', 'id, word, category')
+
+def _connect():
+	conn = sqlite3.connect(_db)
+	c = conn.cursor()
+	return conn, c
 
 def get(category):
-	return data.get(category, [])
+	conn, c = _connect()
+	c.execute("SELECT id, word FROM words WHERE category = ?", (category,))
+	words = [Word(id, word, category) for id, word in c.fetchall()]
+	c.close()
+	conn.close()
+	return words
 
 def add(word, category):
-	data.setdefault(category, []).append(word)
-	print(data)
+	conn, c = _connect()
+	c.execute("INSERT INTO words (word, category) VALUES (?, ?)", (word, category))
+	conn.commit()
+	c.close()
+	conn.close()
+	
+def remove(id):
+	conn, c = _connect()
+	c.execute("DELETE FROM words WHERE id = ?", (id,))
+	conn.commit()
+	c.close()
+	conn.close()
--- german_classifier.py
+++ german_classifier.py
@@ -1,6 +1,7 @@
 import sys
 import time
 from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
 import german_classifier_gui
 import db_helpers
 
@@ -11,14 +12,23 @@ class MainWindow(QMainWindow, german_classifier_gui.Ui_MainWindow):
 		
 		self.categories = ["Articles", "Animals", "Vegetables",
 			"Simple Verbs", "Phrasal Verbs"]
-		# self.data = dict.fromkeys(self.categories, [])
 		self.category_combo.addItems(self.categories)
 		
 		self.submit_button.clicked.connect(self.add)
 		
 		self.tabWidget.currentChanged.connect(self.update_list)
 		
+		for lw in (self.der_list, self.die_list, self.das_list,
+				self.animal_list, self.vegetable_list, self.simple_list,
+				self.phrasal_list):			
+			action = QAction('delete', self)
+			action.triggered.connect(lambda _, lw=lw: self.remove(lw))
+			lw.addAction(action)
+			lw.setContextMenuPolicy(Qt.ActionsContextMenu)
+		
 		self.statusBar().setStyleSheet("color: mediumseagreen")
+		
+		self.setStyleSheet('QMenu::item {color: black}')
 		
 
 	def add(self):
@@ -52,35 +62,66 @@ class MainWindow(QMainWindow, german_classifier_gui.Ui_MainWindow):
 		if category == "Articles": 
 			die = db_helpers.get("die")
 			self.die_list.clear()
-			self.die_list.addItems(die)
+			for id, word, _ in die:
+				item = QListWidgetItem(word)
+				item.setData(Qt.UserRole, id)
+				self.die_list.addItem(item)
 			
 			das = db_helpers.get("das")
 			self.das_list.clear()
-			self.das_list.addItems(das)
+			for id, word, _ in das:
+				item = QListWidgetItem(word)
+				item.setData(Qt.UserRole, id)
+				self.das_list.addItem(item)
 			
 			der = db_helpers.get("der")
 			self.der_list.clear()
-			self.der_list.addItems(der)
+			for id, word, _ in der:
+				item = QListWidgetItem(word)
+				item.setData(Qt.UserRole, id)
+				self.der_list.addItem(item)
 			
 		elif category == "Animals":
 			animals = db_helpers.get("Animals")
 			self.animal_list.clear()
-			self.animal_list.addItems(animals)
+			for id, word, _ in animals:
+				item = QListWidgetItem(word)
+				item.setData(Qt.UserRole, id)
+				self.animal_list.addItem(item)
 			
 		elif category == "Vegetables":
 			vegetables = db_helpers.get("Vegetables")
 			self.vegetable_list.clear()
-			self.vegetable_list.addItems(vegetables)
+			for id, word, _ in vegetables:
+				item = QListWidgetItem(word)
+				item.setData(Qt.UserRole, id)
+				self.vegetable_list.addItem(item)
 			
 		elif category == "Simple Verbs":
 			simple_verbs = db_helpers.get("Simple Verbs")
 			self.simple_list.clear()
-			self.simple_list.addItems(simple_verbs)
+			for id, word, _ in simple_verbs:
+				item = QListWidgetItem(word)
+				item.setData(Qt.UserRole, id)
+				self.simple_list.addItem(item)
 			
 		elif category == "Phrasal Verbs":
 			phrasal_verbs = db_helpers.get("Phrasal Verbs")
 			self.phrasal_list.clear()
-			self.phrasal_list.addItems(phrasal_verbs)
+			for id, word, _ in phrasal_verbs:
+				item = QListWidgetItem(word)
+				item.setData(Qt.UserRole, id)
+				self.phrasal_list.addItem(item)
+		
+	def remove(self, lw):
+		# delete from the list widget
+		row = lw.currentRow()
+		if row == -1: return	# in case of empty list
+		item = lw.takeItem(row)
+		
+		# delete from the database
+		id = item.data(Qt.UserRole)
+		db_helpers.remove(id)
 		
 		
 		
